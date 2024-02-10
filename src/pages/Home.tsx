8c1ed78e63bd5fe489d8342959190010a54b8c4a
jessica-sobreira <j.jecristina@gmail.com>
2024-02-09 21:00:34 -0300

    minhas finanças

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,56 @@
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export function Home() {
     const navigate = useNavigate();
 
     return (
         <>
-        <div>
-            <h1>Home</h1>
-            <Button variant="contained" onClick={() => navigate('/transacoes')}>Cadastrar</Button>
-        </div>
+        <h1>Minhas Finanças</h1>
+        <div style={{
+                display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <br/>
+            <Button variant="contained" onClick={() => navigate('/transacoes')} style = {{color: 'white', backgroundColor: 'darkgreen'}}>Cadastrar</Button>
+            <br/>
+            </div>
+            
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell>Descrição</TableCell>
+                <TableCell align="right">Tipo</TableCell>
+                <TableCell align="right">Valor</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+       
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                Salário
+              </TableCell>
+              <TableCell align="right">Entrada</TableCell>
+              <TableCell align="right">R$ 500</TableCell>
+             
+            </TableRow>
+        
+        </TableBody>
+
+        <TableRow>
+              <TableCell style ={{ fontWeight: 'bold', paddingBottom: 3, paddingTop: 3 }} colSpan={6} >Saldo:</TableCell>
+
+        </TableRow>
+      </Table>
+ 
         </>
     );
 }
+
--- src/pages/Transações.tsx
+++ src/pages/Transações.tsx
@@ -1,12 +1,48 @@
-import { Button } from '@mui/material';
+import { Button, TextField, Box, InputLabel, Select, MenuItem, FormControl } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 export function Transacoes() {
     const navigate = useNavigate();
     return (
         <>
-        <h1>Transacoes</h1>
-        <Button variant="contained" onClick={() => navigate('/')}>Ver Transações</Button>
+        <h1>Finanças</h1>
+        <div style={{
+                display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Button variant="contained" onClick={() => navigate('/')} style = {{color: 'white', backgroundColor: 'green'}}>Ver Transações</Button>
+        </div>
+        <br />
+        <br />
+        <Box>
+        <TextField  id="descricao" label="Descrição" name="descricao" variant="outlined" ></TextField>
+        <br />
+        <br />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Tipo</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={tipo}
+                label="Tipo"
+                // onChange={handleChange}
+            >
+                <MenuItem value={'Entrada'}>Entrada</MenuItem>
+                <MenuItem value={'Saída'}>Saída</MenuItem>
+               
+              </Select>
+          </FormControl>
+        <br />
+        <br />
+        <TextField  id="valor" label="Valor" name="valor" variant="outlined" ></TextField>
+        <br />
+        <br />
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Button variant="contained" style = 
+        {{color: 'white', fontWeight: 'bold', backgroundColor: 'darkgreen'}}>Enviar</Button>
+        </div>
+
+        </Box>
+
+
         </>
     )
 }
